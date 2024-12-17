19edb3111100d7baf732000ee44c7916e7a55a86
tay3434 <tns5371@psu.edu>
2024-12-17 15:02:18 -0500

    switched from dropdowns to inputs

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
-import { WiredButton, WiredCombo, WiredItem, WiredCheckbox, WiredSlider } from "wired-elements";
+import { WiredButton, WiredCombo, WiredItem, WiredCheckbox, WiredSlider, WiredInput } from "wired-elements";
 import "@haxtheweb/rpg-character/rpg-character.js";
 
 export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
@@ -14,10 +14,9 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
     const urlParams = new URLSearchParams(window.location.search);
     const urlseed = urlParams.get('seed');
     console.log(urlseed);
-    if (urlseed){
+    if (urlseed) {
       this.seed = urlseed;
-    }
-    else {
+    } else {
       this.seed = "0000000000";
     }
     this.base = 0;
@@ -43,7 +42,7 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
       fire: { type: Boolean },
       walking: { type: Boolean },
       circle: { type: Boolean },
-      size: { type: Number},
+      size: { type: Number },
       accessories: { type: Number },
       base: { type: Number },
       face: { type: Number },
@@ -52,7 +51,6 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
       pants: { type: Number },
       shirt: { type: Number },
       skin: { type: Number },
-      size: { type: Number },
       hatColor: { type: Number },
       hat: { type: String },
     };
@@ -90,13 +88,12 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
           flex-direction: column;
           align-items: flex-start;
         }
-        wired-item{
+        wired-item {
           opacity: 1;
         }
         wired-combo {
           flex: 1;
           width: 100%;
-          opacity: 1;
         }
         wired-checkbox {
           display: block;
@@ -114,7 +111,6 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
         wired-input {
           width: 100%;
           margin-bottom: var(--ddd-spacing-4);
-          opacity: 1;
         }
         .seed-display {
           margin-top: var(--ddd-spacing-2);
@@ -150,35 +146,24 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
         </div>
         <div class="inputs-panel">
           <h2>Customize Your Character</h2>
-          <label for="accessories">Accessories:</label>
-          <wired-input type="number" min="0" max="9" value="${this.accessories}" @input="${this.updateProperty('accessories')}"></wired-input>
-
-          <label for="base">Base:</label>
-          <wired-input type="number" min="0" max="9" value="${this.base}" @input="${this.updateProperty('base')}"></wired-input>
-
-          <label for="face">Face:</label>
-          <wired-input type="number" min="0" max="9" value="${this.face}" @input="${this.updateProperty('face')}"></wired-input>
-
-          <label for="faceItem">Face Item:</label>
-          <wired-input type="number" min="0" max="9" value="${this.faceItem}" @input="${this.updateProperty('faceItem')}"></wired-input>
-
-          <label for="hair">Hair:</label>
-          <wired-input type="number" min="0" max="9" value="${this.hair}" @input="${this.updateProperty('hair')}"></wired-input>
-
-          <label for="pants">Pants:</label>
-          <wired-input type="number" min="0" max="9" value="${this.pants}" @input="${this.updateProperty('pants')}"></wired-input>
-
-          <label for="shirt">Shirt:</label>
-          <wired-input type="number" min="0" max="9" value="${this.shirt}" @input="${this.updateProperty('shirt')}"></wired-input>
-
-          <label for="skin">Skin:</label>
-          <wired-input type="number" min="0" max="9" value="${this.skin}" @input="${this.updateProperty('skin')}"></wired-input>
-
-          <label for="hatColor">Hat Color:</label>
-          <wired-input type="number" min="0" max="9" value="${this.hatColor}" @input="${this.updateProperty('hatColor')}"></wired-input>
-
+          ${this._renderNumericInputRow([
+            { label: "Accessories", key: "accessories" },
+            { label: "Base", key: "base" },
+          ])}
+          ${this._renderNumericInputRow([
+            { label: "Face", key: "face" },
+            { label: "Face Item", key: "faceItem" },
+          ])}
+          ${this._renderNumericInputRow([
+            { label: "Hair", key: "hair" },
+            { label: "Pants", key: "pants" },
+          ])}
+          ${this._renderNumericInputRow([
+            { label: "Shirt", key: "shirt" },
+            { label: "Skin", key: "skin" },
+          ])}
           <label for="hat">Hat:</label>
-          <!-- <select value="${this.hat}" @change="${this.updateProperty('hat')}">
+          <wired-combo value="${this.hat}" @change="${this.updateProperty('hat')}">
             <option value="none" ?selected="${this.hat === 'none'}">None</option>
             <option value="bunny" ?selected="${this.hat === 'bunny'}">Bunny</option>
             <option value="coffee" ?selected="${this.hat === 'coffee'}">Coffee</option>
@@ -190,20 +175,7 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
             <option value="party" ?selected="${this.hat === 'party'}">Party</option>
             <option value="pirate" ?selected="${this.hat === 'pirate'}">Pirate</option>
             <option value="watermelon" ?selected="${this.hat === 'watermelon'}">Watermelon</option>
-        </select> -->
-        <wired-combo value="${this.hat}" @change="${this.updateProperty('hat')}">
-            <wired-item value="none" ?selected="${this.hat === 'none'}">None</wired-item>
-            <wired-item value="bunny" ?selected="${this.hat === 'bunny'}">Bunny</wired-item>
-            <wired-item value="coffee" ?selected="${this.hat === 'coffee'}">Coffee</wired-item>
-            <wired-item value="construction" ?selected="${this.hat === 'construction'}">Construction</wired-item>
-            <wired-item value="cowboy" ?selected="${this.hat === 'cowboy'}">Cowboy</wired-item>
-            <wired-item value="education" ?selected="${this.hat === 'education'}">Education</wired-item>
-            <wired-item value="knight" ?selected="${this.hat === 'knight'}">Knight</wired-item>
-            <wired-item value="ninja" ?selected="${this.hat === 'ninja'}">Ninja</wired-item>
-            <wired-item value="party" ?selected="${this.hat === 'party'}">Party</wired-item>
-            <wired-item value="pirate" ?selected="${this.hat === 'pirate'}">Pirate</wired-item>
-            <wired-item value="watermelon" ?selected="${this.hat === 'watermelon'}">Watermelon</wired-item>
-  </wired-combo>
+          </wired-combo>
           <label for="size">Character Size:</label>
           <wired-slider
             id="size"
@@ -211,7 +183,7 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
             min="100"
             max="600"
             @change="${this._updateSize}"
-            ></wired-slider>
+          ></wired-slider>
           ${this._renderCheckbox("Fire", "fire")}
           ${this._renderCheckbox("Walking", "walking")}
           ${this._renderCheckbox("Circle", "circle")}
@@ -221,40 +193,26 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
     `;
   }
 
-  // _renderDropdownRow(dropdowns) {
-  //   return html`
-  //     <div class="dropdown-row">
-  //       ${dropdowns.map((dropdown) =>
-  //         html`<div class="dropdown-container">
-  //           <label>${dropdown.label}</label>
-  //           ${dropdown.values
-  //             ? this._renderDropdownWithValues(dropdown.key, dropdown.values)
-  //             : this._renderDropdown(dropdown.key, dropdown.range)}
-  //         </div>`
-  //       )}
-  //     </div>
-  //   `;
-  // }
-
-  // _renderDropdown(key, range) {
-  //   return html`
-  //     <wired-combo
-  //       @selected="${(e) => this._updateSeed(key, e.detail.selected)}"
-  //     >
-  //       ${Array.from({ length: range }, (_, i) => html`<wired-item value="${i}">${i}</wired-item>`)}
-  //     </wired-combo>
-  //   `;
-  // }
-
-  // _renderDropdownWithValues(key, values) {
-  //   return html`
-  //     <wired-combo
-  //       @selected="${(e) => this._updateSeed(key, e.detail.value)}"
-  //     >
-  //       ${values.map((value) => html`<wired-item value="${value}">${value}</wired-item>`)}
-  //     </wired-combo>
-  //   `;
-  // }
+  _renderNumericInputRow(inputs) {
+    return html`
+      <div class="dropdown-row">
+        ${inputs.map(
+          (input) => html`
+            <div class="dropdown-container">
+              <label>${input.label}</label>
+              <wired-input
+                type="number"
+                min="0"
+                max="9"
+                .value="${this[input.key]}"
+                @input="${this.updateProperty(input.key)}"
+              ></wired-input>
+            </div>
+          `
+        )}
+      </div>
+    `;
+  }
 
   _renderCheckbox(label, key) {
     return html`
@@ -267,39 +225,6 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
     `;
   }
 
-  _updateSeed(key, value) {
-    const seedArray = this.seed.padEnd(10, "0").split("");
-    const index = this._getSeedIndex(key);
-    if (index >= 0) {
-      seedArray[index] = value;
-      this.seed = seedArray.join("");
-    }
-  }
-
-  _getSeedIndex(key) {
-    const mapping = {
-      accessories: 0,
-      base: 1,
-      face: 2,
-      faceItem: 3,
-      hair: 4,
-      pants: 5,
-      shirt: 6,
-      skin: 7,
-      hatColor: 8,
-    };
-    return mapping[key] ?? -1;
-  }
-
-  _updateCheckbox(key, value) {
-    this[key] = value;
-  }
-
-  _generateShareLink() {
-    const link = `${location.origin}${location.pathname}?seed=${this.seed}&hat=${this.hat}&fire=${this.fire}&walking=${this.walking}&circle=${this.circle}`;
-    navigator.clipboard.writeText(link);
-    alert("Link copied to clipboard!");
-  }
   updateProperty(property) {
     return (e) => {
       const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
@@ -309,19 +234,6 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
   }
 
   _updateSize(e) {
-    const newsize = e.detail; // add .value
-    // console.log(`Size updated to: ${newsize}`);
-    // this.size = newsize;
-
-    // // Ensure that the character size is reflected in the custom property
-    // const character = this.shadowRoot.querySelector("rpg-character");
-    // if (character) {
-    //   character.style.setProperty("--character-size", `${newsize}px`);
-    }
+    const newSize = e.detail
   }
-
-globalThis.customElements.define(RpgMe.tag, RpgMe);
-
-
-   
-
+}
